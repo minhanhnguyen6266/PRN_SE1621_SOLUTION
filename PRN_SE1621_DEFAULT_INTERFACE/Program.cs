b89910866aa4d3fc7953233ebf69a0eff77f3bec
minhanhnguyen6266 <minhanhnguyen6266@gmail.com>
2022-05-20 09:08:58 +0700

    Add project files.

---
+++ PRN_SE1621_DEFAULT_INTERFACE/Program.cs
@@ -0,0 +1,84 @@
+﻿
+namespace PRN.SE1621.DEFAULT_INTERFACE;
+
+using static System.Console;
+using System.Collections;
+/*default interface*/
+public interface ISample
+{
+    static void Print()
+    {
+        Console.WriteLine($"Hello .NET C# 10");    
+    }
+
+    string  GetString(string s)
+    {
+        return "Welcome " + s;
+    }
+
+    void Display(); 
+}
+
+public interface ISecond
+{
+    string GetString(string s)
+    {
+        return "Goodmorning " + s;
+    }
+}
+
+public class MySecond : ISecond
+{
+
+}
+
+public class MySample : ISample
+{
+    public void Display()
+    {
+        WriteLine($"Hi !");
+    }
+}
+
+public class MyClass
+{
+    public static int x = 1;
+    static MyClass()
+    {
+        x = 2;
+        Console.WriteLine($"Static constructor {x}");
+    }
+
+    public MyClass()
+    {
+        x++;
+        Console.WriteLine($"Public constructor {x}");
+    }
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        /*
+        MySample obj = new MySample(); //instance
+        obj.Display();
+
+        ISample.Print();
+        // polymorphism
+        ISample s = obj;
+        String str = s.GetString("Dong");
+
+        WriteLine(str); 
+        s.Display();
+        */
+
+        MyClass m1 = new MyClass();
+        MyClass.x = 4;
+        MyClass m2 = new MyClass();
+        
+        
+        ReadLine();
+    }
+}
---
+++ PRN_SE1621_PRO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Fptedu.Prn1621;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"========================");
+        int a = 10, b = 20; //arguments
+        //Console.WriteLine("Total of a = {0} and b = {1} is {2}", a, b, Sum(a, b)");
+        Console.WriteLine($"Total of a = {a} and b = {b} is: {Sum(a,b)}"); // String Interpolation
+        Console.ReadLine();
+    }
+
+    // manual method: parameters
+    public static int Sum(int x, int y)
+    {
+        return x + y;
+    }
+}
+
+
+
